4922b21cd7f967e267af52b059752b623baea41a
rfding <rita.ding@connexta.com>
2019-03-14 15:35:36 -0400

    first commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,29 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @RequestMapping("/")
+    public String home() {
+        logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warn message");
+        logger.error("This is an error message");
+        return "Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
